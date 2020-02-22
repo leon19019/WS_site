34abddb0b92c2f50a2f2d12169b0ae8e4bb81053
CoffeBee <ivan.podvorniy@yandex.ru>
2020-02-22 11:49:58 +0300

    flask file

--- flask.py
+++ flask.py
@@ -1,5 +1,10 @@
-from flask import Flask
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def hello_world():
+    return render_template('complete_WS_Site.html')
